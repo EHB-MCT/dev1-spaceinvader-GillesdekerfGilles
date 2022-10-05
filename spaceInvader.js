0522d947c7512a3a3f810090e8aed4692ba54982
Gilles De Kerf <Gilles.De.kerf@student.ehb.be>
2022-10-05 13:30:55 +0200

    Space Invader Gilles De Kerf

--- spaceInvader.js
+++ spaceInvader.js
@@ -1,3 +1,49 @@
 "use strict";
 
-alert("Draw your space invader here");
+let canvas = document.querySelector("canvas");
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+let context = canvas.getContext("2d");
+
+SpaceInvader();
+
+
+function SpaceInvader() {
+    context.beginPath();
+    context.rect(50, 50, 300, 300);
+    context.fillStyle = "black";
+    context.fill();
+    context.stroke();
+
+    context.beginPath();
+    context.rect(75, 225, 250, 100);
+    context.fillStyle = "#66FF00";
+    context.fill();
+    context.stroke();
+
+    context.beginPath();
+    context.rect(175, 225, 50, 50);
+    context.fillStyle = "black";
+    context.fill();
+    context.stroke();
+
+    context.beginPath();
+    context.rect(175, 125, 50, 50);
+    context.fillStyle = "#66FF00";
+    context.fill();
+    context.stroke();
+
+    context.beginPath();
+    context.rect(75, 75, 50, 50);
+    context.fillStyle = "#66FF00";
+    context.fill();
+    context.stroke();
+
+    context.beginPath();
+    context.rect(275, 75, 50, 50);
+    context.fillStyle = "#66FF00";
+    context.fill();
+    context.stroke();
+
+
+}
